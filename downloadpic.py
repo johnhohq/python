56573c8ff55d862723360709f9057e2c032a1fc3
johnhohq <523881376@qq.com>
2018-03-18 21:05:00 +0800

    Update downloadpic.py

--- downloadpic.py
+++ downloadpic.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# __Author__: 陌路疏途
+# __Author__: John Ho
 
 
 # 四部曲：1.导入模块 2.获取网页源码 3.正则匹配下载 4.调用函数
 
-import urllib.request #获取网址模块
-import re  #正则匹配模块
+#获取网址模块
+import urllib.request
+
+#正则匹配模块
+import re
 
 #定义获取网页源代码函数
 def gethtml():
@@ -16,15 +19,13 @@ def gethtml():
     return html
 
 #匹配
-
 def getimg(html):
     imgre = re.compile(r' src="(.*?)" class=')#正则匹配，compile为把正则表达式编译成一个正则表达式对象，提供效率。
     imglist = re.findall(imgre, html)#获取字符串中所有匹配的字符串
     x = 0 #定义全局变量默认为0
     for imgurl in imglist: #循环图片字符串列表并输出
         print(imgurl)
-
-#下载
+        #下载
         urllib.request.urlretrieve(imgurl,'%s.jpg' % x)#把图片下载到本地并指定保存目录
         x += 1 #每次自增1
         print("正在下载第%s张" % x)#格式化输出张数
