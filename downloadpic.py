68262efee9376069f8967acb6aa03937d50a2e6b
johnhohq <523881376@qq.com>
2018-03-18 21:08:04 +0800

    Update downloadpic.py

--- downloadpic.py
+++ downloadpic.py
@@ -1,3 +1,8 @@
+''''' 
+爬取http://www.wmpic.me/tupian/love图片文件链接
+保存图片到当前目录
+''' 
+
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # __Author__: John Ho
@@ -13,7 +18,7 @@ import re
 
 #定义获取网页源代码函数
 def gethtml():
-    papg = urllib.request.urlopen('http://www.wmpic.me/tupian/cute') #打开图片的网址
+    papg = urllib.request.urlopen('http://www.wmpic.me/tupian/love') #打开图片的网址
     html = papg.read()  #用read方法读成网页源代码，格式为字节对象
     html = html.decode('UTF-8') #定义编码格式解码字符串(字节转换为字符串)
     return html
@@ -28,7 +33,7 @@ def getimg(html):
         #下载
         urllib.request.urlretrieve(imgurl,'%s.jpg' % x)#把图片下载到本地并指定保存目录
         x += 1 #每次自增1
-        print("正在下载第%s张" % x)#格式化输出张数
+        print("Downloading picture no %s" % x)#格式化输出张数
 
 #调用函数
 html = gethtml()
